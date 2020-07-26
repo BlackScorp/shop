6d1f40f2d07088a348ac79a12ad6ee0d307ef13d
BlackScorp <vitalij@blackscorp.de>
2020-07-26 13:24:44 +0200

    rechnungs template bearbeitet und firmen daten ausgegeben

--- templates/invoice.php
+++ templates/invoice.php
@@ -1,4 +1,15 @@
 <div class="pdf-container">
+<section class="row" id="companyLogo">
+</section>
+<section class="row" id="companyDetails">
+  <?= COMPANY_NAME ?> | <?= COMPANY_STREET ?> | <?= COMPANY_ZIP ?> <?= COMPANY_CITY?>
+</section>
+<section class="row" id="invoiceAddress">
+</section>
+<section class="row" id="invoiceDetails">
+</section>
+<section class="row" id="invoideHeader">
+</section>
 <section id="products">
   <?php foreach($orderData['products'] as $order):?>
     <?=$order['title']?>
@@ -7,4 +18,11 @@
           <?=$order['taxInPercent']?>
   <?php  endforeach; ?>
 </section>
+<section class="row" id="sum">
+</section>
+<section class="row" id="invoiceDetailsFooter">
+</section>
+<section class="row" id="footer">
+</section>
+
 </div>
