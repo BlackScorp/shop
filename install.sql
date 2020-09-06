21cb28feaccc84d07fccb0669c866556baf1938e
BlackScorp <vitalij@blackscorp.de>
2020-09-06 14:07:20 +0200

    install.sql fixed

--- install.sql
+++ install.sql
@@ -1,141 +1,287 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server Version:               10.1.20-MariaDB - mariadb.org binary distribution
--- Server Betriebssystem:        Win64
--- HeidiSQL Version:             11.0.0.5919
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 06. Sep 2020 um 14:05
+-- Server-Version: 10.4.14-MariaDB
+-- PHP-Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Exportiere Struktur von Tabelle shop.cart
+--
+-- Datenbank: `shop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `cart`
+--
+
 DROP TABLE IF EXISTS `cart`;
-CREATE TABLE IF NOT EXISTS `cart` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL DEFAULT '0',
-  `product_id` int(10) unsigned DEFAULT NULL,
-  `quantity` int(10) unsigned NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `product_id_user_id` (`product_id`,`user_id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `FK_cart_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `cart` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `product_id` int(10) UNSIGNED DEFAULT NULL,
+  `quantity` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Exportiere Daten aus Tabelle shop.cart: ~2 rows (ungefähr)
-DELETE FROM `cart`;
-/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
-INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`, `created`) VALUES
-	(1, 1, 1, 1, '2020-04-26 14:14:18'),
-	(2, 1, 2, 1, '2020-04-26 14:14:20');
-/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Exportiere Struktur von Tabelle shop.delivery_adresses
+--
+-- Tabellenstruktur für Tabelle `delivery_adresses`
+--
+
 DROP TABLE IF EXISTS `delivery_adresses`;
-CREATE TABLE IF NOT EXISTS `delivery_adresses` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) unsigned NOT NULL,
+CREATE TABLE `delivery_adresses` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL,
   `recipient` text NOT NULL,
   `city` text NOT NULL,
   `street` text NOT NULL,
   `streetNumber` varchar(50) NOT NULL,
-  `zipCode` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_user_delivery_addresses` (`user_id`),
-  CONSTRAINT `FK_user_delivery_addresses` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `zipCode` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Exportiere Daten aus Tabelle shop.delivery_adresses: ~0 rows (ungefähr)
-DELETE FROM `delivery_adresses`;
-/*!40000 ALTER TABLE `delivery_adresses` DISABLE KEYS */;
-/*!40000 ALTER TABLE `delivery_adresses` ENABLE KEYS */;
+-- --------------------------------------------------------
 
--- Exportiere Struktur von Tabelle shop.products
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(191) CHARACTER SET utf8 NOT NULL,
-  `description` text CHARACTER SET utf8 NOT NULL,
-  `price` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+--
+-- Tabellenstruktur für Tabelle `orders`
+--
 
--- Exportiere Daten aus Tabelle shop.products: ~4 rows (ungefähr)
-DELETE FROM `products`;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` (`id`, `title`, `description`, `price`) VALUES
-	(1, 'Produkt 1', 'Tolles Produkt', 166),
-	(2, 'Produkt 2', 'Anderes Produkt', 1337),
-	(3, 'Produkt 3', 'Noch mehr Produkte', 42),
-	(4, 'Produkt 4', 'Alle guten Dinge sind 4', 2020);
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-
--- Exportiere Struktur von Tabelle shop.user
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(191) NOT NULL DEFAULT '0',
-  `password` varchar(191) NOT NULL DEFAULT '0',
-  `email` varchar(512) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
-
--- Exportiere Daten aus Tabelle shop.user: ~1 rows (ungefähr)
-DELETE FROM `user`;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-	(1, 'test', '$2y$10$/wFthkyVlBK4fkrCoggdQuHdqLDZrlBRglk2g898Lw/ggrHdJDxwa');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-
--- Exportiere Struktur von Tabelle shop.orders
 DROP TABLE IF EXISTS `orders`;
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `orders` (
+  `id` int(10) UNSIGNED NOT NULL,
   `orderDate` date NOT NULL DEFAULT '0000-00-00',
   `deliveryDate` date NOT NULL DEFAULT '0000-00-00',
   `status` enum('new','canceled','payed','sent','delivered') NOT NULL DEFAULT 'new',
-  `userId` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `orderDate` (`orderDate`),
-  KEY `status` (`status`),
-  KEY `userId` (`userId`),
-  CONSTRAINT `FK_ORDER_TO_USER` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `userId` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
 
--- Exportiere Struktur von Tabelle shop.order_products
-DROP TABLE IF EXISTS `order_products`;
-CREATE TABLE IF NOT EXISTS `order_products` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) NOT NULL DEFAULT '',
-  `quantity` int(10) unsigned NOT NULL DEFAULT '0',
-  `price` int(10) unsigned NOT NULL DEFAULT '0',
-  `taxInPercent` int(10) unsigned NOT NULL DEFAULT '0',
-  `orderId` int(10) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `FK_ODERS_TO_ORDER_PRODUCTS` (`orderId`),
-  CONSTRAINT `FK_ODERS_TO_ORDER_PRODUCTS` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+--
+-- Tabellenstruktur für Tabelle `order_adresses`
+--
 
-CREATE TABLE IF NOT EXISTS `order_adresses` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `order_id` int(11) unsigned NOT NULL,
+DROP TABLE IF EXISTS `order_adresses`;
+CREATE TABLE `order_adresses` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `order_id` int(11) UNSIGNED NOT NULL,
   `recipient` mediumtext NOT NULL,
   `city` text NOT NULL,
   `street` text NOT NULL,
   `streetNumber` varchar(50) NOT NULL,
   `zipCode` varchar(50) NOT NULL,
-  `type` enum('both','delivery','invoice') DEFAULT 'both',
-  PRIMARY KEY (`id`),
-  KEY `FK_ORDER_ADRESS` (`order_id`),
-  KEY `type` (`type`),
-  CONSTRAINT `FK_ORDER_ADRESS` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  `type` enum('both','delivery','invoice') DEFAULT 'both'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `order_products`
+--
+
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE `order_products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(50) NOT NULL DEFAULT '',
+  `quantity` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `price` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `taxInPercent` int(10) UNSIGNED NOT NULL DEFAULT 0,
+  `orderId` int(10) UNSIGNED NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(191) CHARACTER SET utf8 NOT NULL,
+  `description` text CHARACTER SET utf8 NOT NULL,
+  `price` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- TRUNCATE Tabelle vor dem Einfügen `products`
+--
+
+TRUNCATE TABLE `products`;
+--
+-- Daten für Tabelle `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `description`, `price`) VALUES
+(1, 'Produkt 1', 'Tolles Produkt', 166),
+(2, 'Produkt 2', 'Anderes Produkt', 1337),
+(3, 'Produkt 3', 'Noch mehr Produkte', 42),
+(4, 'Produkt 4', 'Alle guten Dinge sind 4', 2020);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(191) NOT NULL DEFAULT '0',
+  `password` varchar(191) NOT NULL DEFAULT '0',
+  `email` varchar(512) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `product_id_user_id` (`product_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indizes für die Tabelle `delivery_adresses`
+--
+ALTER TABLE `delivery_adresses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_user_delivery_addresses` (`user_id`);
+
+--
+-- Indizes für die Tabelle `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orderDate` (`orderDate`),
+  ADD KEY `status` (`status`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indizes für die Tabelle `order_adresses`
+--
+ALTER TABLE `order_adresses`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_ORDER_ADRESS` (`order_id`),
+  ADD KEY `type` (`type`);
+
+--
+-- Indizes für die Tabelle `order_products`
+--
+ALTER TABLE `order_products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_ODERS_TO_ORDER_PRODUCTS` (`orderId`);
+
+--
+-- Indizes für die Tabelle `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indizes für die Tabelle `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `delivery_adresses`
+--
+ALTER TABLE `delivery_adresses`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `order_adresses`
+--
+ALTER TABLE `order_adresses`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `order_products`
+--
+ALTER TABLE `order_products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT für Tabelle `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints der exportierten Tabellen
+--
+
+--
+-- Constraints der Tabelle `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `FK_cart_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints der Tabelle `delivery_adresses`
+--
+ALTER TABLE `delivery_adresses`
+  ADD CONSTRAINT `FK_user_delivery_addresses` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints der Tabelle `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `FK_ORDER_TO_USER` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints der Tabelle `order_adresses`
+--
+ALTER TABLE `order_adresses`
+  ADD CONSTRAINT `FK_ORDER_ADRESS` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints der Tabelle `order_products`
+--
+ALTER TABLE `order_products`
+  ADD CONSTRAINT `FK_ODERS_TO_ORDER_PRODUCTS` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
