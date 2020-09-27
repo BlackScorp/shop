9f293430613e68b77974cd9b3adcf916d325ea8b
BlackScorp <vitalij@blackscorp.de>
2020-09-27 12:33:29 +0200

    neue konstante LOG_DIR

--- index.php
+++ index.php
@@ -4,5 +4,6 @@ error_reporting(-1);
 ini_set('display_errors','On');
 
 define('CONFIG_DIR',__DIR__.'/config');
+define('LOG_DIR',__DIR__.'/logs');
 define('ASSETS_DIR',__DIR__.'/assets');
 require __DIR__.'/includes.php';
