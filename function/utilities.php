0bae504420f1bca003a094d81e535ae4dfc6e9db
BlackScorp <cccpmik@gmail.com>
2020-10-17 19:37:05 +0200

    log dir erstellen, wenn es nicht exestiert

--- function/utilities.php
+++ function/utilities.php
@@ -57,6 +57,9 @@ function logData(string $level,string $message,?array $data = null){
 
   $today = date('Y-m-d');
   $now = date('Y-m-d H:i:s');
+  if(!is_dir(LOG_DIR)){
+    mkdir(LOG_DIR,0777,true);
+  }
   $logFile = LOG_DIR.'/log-'.$today.'.log';
 
   $logData = '['.$now.'-'.$level.'] '.$message."\n";
