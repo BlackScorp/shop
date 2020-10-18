18046e17637c5f4e6f81355870a85d55643f9896
BlackScorp <cccpmik@gmail.com>
2020-10-18 13:50:20 +0200

    flash Message ausgelagert

---
+++ templates/flashMessage.php
@@ -0,0 +1,7 @@
+<?php if($hasFlashMessages):?>
+  <div class="alert alert-success" role="alert">
+    <?php foreach($flashMessages as $message):?>
+        <p><?= $message?></p>
+    <?php endforeach?>
+  </div>
+<?php endif;?>
--- templates/main.php
+++ templates/main.php
@@ -1,12 +1,6 @@
 <?php require_once __DIR__.'/header.php'?>
 <section class="container" id="products">
-  <?php if($hasFlashMessages):?>
-  <div class="alert alert-success" role="alert">
-    <?php foreach($flashMessages as $message):?>
-        <p><?= $message?></p>
-    <?php endforeach?>
-  </div>
-<?php endif;?>
+  <?php require_once __DIR__.'/flashMessage.php' ?>
   <div class="row">
   <?php if($isAdmin):?>
     <div class="col">
