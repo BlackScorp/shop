b190e893d026aec2953f2bfadb5fadf964590571
BlackScorp <cccpmik@gmail.com>
2020-10-18 13:50:46 +0200

    neues Produkt formular erstellt

--- templates/newProduct.php
+++ templates/newProduct.php
@@ -6,7 +6,24 @@
             Neue produkt erstellen
 </div>
 <div class="card-body">
-    ..
+<?php require_once __DIR__.'/flashMessage.php' ?>
+<?php require_once __DIR__.'/errorMessages.php'?>
+    <div class="form-group">
+        <label for="name">Produkt Name</label>
+        <input type="text" value="<?=escape($productName)?>" name="name" id="name" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="slug">Slug</label>
+        <input type="text" value="<?=escape($slug)?>" name="slug" id="slug" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="description">Produkt Beschreibung</label>
+        <textarea class="form-control" id="description" name="description" rows="3"><?=escape($description)?></textarea>
+    </div>
+    <div class="form-group">
+        <label for="price">Price</label>
+        <input type="text" value="<?=escape($price)?>" name="price" id="price" class="form-control">
+    </div>
 </div>
 <div class="card-footer">
     <a href="index.php" class="btn btn-danger">Abbrechen</a>
