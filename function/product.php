ecf0db90f8e3a75c49af68261a7b79ae2e455219
BlackScorp <cccpmik@gmail.com>
2020-10-18 13:51:23 +0200

    - produkt anlegen funktion - nut live produkte anzeigen

--- function/product.php
+++ function/product.php
@@ -2,7 +2,8 @@
 
 function getAllProducts(){
   $sql ="SELECT id,title,description,price,slug
-  FROM products";
+  FROM products
+  WHERE status = 'LIVE'";
 
   $result = getDB()->query($sql);
   if(!$result){
@@ -32,4 +33,27 @@ function getProductBySlug(string $slug):?array{
     return null;
   }
   return $statement->fetch();
+}
+
+function createProduct(string $productName,string $slug,string $description,int $price):bool{
+    $sql="INSERT INTO products SET 
+    title = :productName,
+    slug = :slug,
+    description = :description,
+    price = :price
+    ";
+      $statement = getDB()->prepare($sql);
+      if(false === $statement){
+        return false;
+      }
+      $statement->execute(
+        [
+          ':productName' => $productName,
+          ':slug'=>$slug,
+          ':description'=>$description,
+          ':price'=>$price,
+        ]
+      ); 
+      $lastId = getDB()->lastInsertId();
+      return $lastId > 0;
 }
