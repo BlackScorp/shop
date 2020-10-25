c61e7a409942467eda2a7b58f648d2d3bae84d6c
BlackScorp <cccpmik@gmail.com>
2020-10-25 17:52:26 +0100

    produkt editieren button angelegt

--- templates/card.php
+++ templates/card.php
@@ -1,4 +1,4 @@
-<div class="card">
+<div class="card <?=$product['status']?>">
   <div class="card-title"><?= $product['title']?></div>
   <img src="https://placekitten.com/286/180" class="card-img-top" alt="produkt">
   <div class="card-body">
@@ -9,5 +9,8 @@
   <div class="card-footer">
     <a href="index.php/product/<?= $product['slug']?>" class="btn btn-primary btn-sm">details</a>
     <a href="index.php/cart/add/<?= $product['id']?>" class="btn btn-success btn-sm">In den Warenkorb</a>
+    <?php if($isAdmin):?>
+      <a href="index.php/product/edit/<?= $product['slug']?>" class="btn btn-primary btn-sm">bearbeiten</a>
+    <?php endif;?>
   </div>
 </div>
