53d02fa2c10f7356d7013f52a1a67a7cf363c698
BlackScorp <cccpmik@gmail.com>
2020-11-01 18:04:53 +0100

    rechnung kann mit security key angeschaut werden

--- actions/invoice.php
+++ actions/invoice.php
@@ -1,18 +1,45 @@
 <?php
 
-redirectIfNotLogged('/');
+
 $routeParts =  explode('/',$route);
   $invoiceId = null;
+  $securityKey = null;
+  
 if(isset($routeParts[2])){
   $invoiceId=(int)$routeParts[2];
+}
+
+if(isset($routeParts[3])){
+  $securityParts = explode('.',$routeParts[3]);
+  if(count($securityParts) === 2){
+   
+    $securityKeyPath = $securityParts[0];
+    $securityKeyCheck = $securityParts[1];
+    $securityKeyFileName = STORAGE_DIR.'/security/key_'.$securityKeyPath.'.txt';
+    if(is_file($securityKeyFileName)){
+      $checkKey = file_get_contents($securityKeyFileName);
+      if($securityKeyCheck === $checkKey){
+        $securityKey = $routeParts[3];
+      }
+    }
+  }
+}
+if(!$securityKey){
+redirectIfNotLogged('/');
 }
 if(!$invoiceId){
 echo "Rechnung nicht angegeben";
   exit();
 }
+
 $userId = getCurrentUserId();
+if($securityKey){
+  $userId = null;
+}
 
 $orderData = getOrderForUser($invoiceId,$userId);
+
+$userId = $orderData['userId'];
 $orderSum = getOrderSumForUser($invoiceId,$userId);
 $userData = getUserDataForId($userId);
 if(!$orderData){
