04ad91b3d9aa93a0268a162617c5a635477feb7c
Blackscorp <cccpmik@gmail.com>
2021-01-24 14:21:21 +0100

    categories mit kind catogies werden angezeigt

--- function/category.php
+++ function/category.php
@@ -1,16 +1,21 @@
 <?php
 
-function getCategories():array{
+function getCategories(?int $categoryId):array{
     $categories = [];
-    $sql ="SELECT id,label,parentId
-    FROM categories
-    ORDER BY `position`,label";
+    $sql ="SELECT id,label,parentId,id=:categoryId as isPrimary
+    FROM categories WHERE ";
+    $tempSql = "parentId is null ";
+    if($categoryId){
+        $tempSql ="id=:categoryId OR parentId = :categoryId ";
+    }
+    $sql .=$tempSql;
+    $sql .="ORDER BY isPrimary DESC,`position`,label";
 
     $statement = getDB()->prepare($sql);
     if(false === $statement){
       return $categories;
     }
-    $statement->execute();
+    $statement->execute([':categoryId'=>$categoryId]);
     if(0 === $statement->rowCount()){
         return $categories;
     }
