03f89100deec1c92936fa4e22ff7afa189264a22
SYS64738 <sys64738@sys64738.de>
2021-02-06 08:51:50 +0100

    [TASK] Update composer.lock; Verbesserte Lesbarkeit

--- function/database.php
+++ function/database.php
@@ -6,7 +6,7 @@ function getDB(){
     return $db;
   }
   require_once CONFIG_DIR.'/database.php';
-  $dsn = sprintf("mysql:host=%s;port=%d;dbname=%s;charset=%s",DB_HOST, DB_PORT,DB_DATABASE,DB_CHARSET);
+  $dsn = sprintf("mysql:host=%s;port=%d;dbname=%s;charset=%s",DB_HOST, DB_PORT, DB_DATABASE, DB_CHARSET);
   $db = new PDO($dsn,DB_USERNAME,DB_PASSWORD);
   return $db;
 }
