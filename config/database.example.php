449c42a30c92f6e1d34f7e039f9cb8761de2126d
Blackscorp <cccpmik@gmail.com>
2021-02-07 19:45:44 +0100

    add example configs

---
+++ config/database.example.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_DATABASE','shop');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_HOST','127.0.0.1');
+define('DB_PORT','3306');
+define('DB_CHARSET','utf8');
---
+++ config/mail.example.php
@@ -0,0 +1,7 @@
+<?php
+define('SMTP_USERNAME','');
+define('SMTP_PASSWORD','');
+define('MAIL_NOREPLY','noreply@shop.com');
+define('SMTP_HOST','smtp.gmail.com');
+define('SMPT_PORT','465');
+define('SMTP_SSL','ssl');
